fe2c2001099790883909001b37802e7a447ff34a
Samuel Souza <samuel_f2@icloud.com>
2024-10-08 17:37:02 -0300

    feat: add drawer and style

--- components/deckForm.tsx
+++ components/deckForm.tsx
@@ -4,6 +4,16 @@ import { setPlayerCard } from "@/app/actions"
 
 import { useState } from "react"
 
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+} from "@/components/ui/drawer"
+
 type DeckFormProps = {
     playerDeck: string[]
 }
@@ -21,21 +31,38 @@ const DeckForm = ({ playerDeck }: DeckFormProps) => {
         alert(`Enviado: ${selectedCard}`)
     }
 
+    const handleClick = () => {
+        document.querySelector('form')?.requestSubmit()
+    }
+
     return (
         <>
-            <form action={handleSubmit}>
-                {playerDeck.map((item, key) => (
-                    <label className="text-black" key={key}>
-                        <input
-                            type="radio"
-                            name="card"
-                            value={item}
-                        />
-                        {item}
-                    </label>
-                ))}
-                <button type="submit" className="text-black bg-white">Choose</button>
-            </form>
+            <Drawer>
+                <DrawerTrigger>Open</DrawerTrigger>
+                <DrawerContent>
+                    <DrawerHeader>
+                        <DrawerTitle>Qual atrocidade você dirá hoje?</DrawerTitle>
+                        <DrawerDescription className="text-neutral-600">Lembrando que quanto pior, melhor.</DrawerDescription>
+                    </DrawerHeader>
+                    <form action={handleSubmit} className="px-4 flex flex-col gap-4 pb-4">
+                        <div className="grid grid-cols-2 gap-4">
+                            {playerDeck.map((item, key) => (
+                                <DrawerClose className="w-full flex" key={key}>
+                                    <label onClick={handleClick} className="w-full text-black text-start border border-neutral-300 has-[:checked]:border-purple-900 p-4 hover:border-neutral-900 hover:bg-neutral-200/50">
+                                        <input
+                                            className="hidden"
+                                            type="radio"
+                                            name="card"
+                                            value={item}
+                                        />
+                                        {item}
+                                    </label>
+                                </DrawerClose>
+                            ))}
+                        </div>
+                    </form>
+                </DrawerContent>
+            </Drawer>
             <span>{selectedCard}</span>
             <button disabled={!selectedCard} onClick={handleSendCard}>Send Card</button>
         </>
